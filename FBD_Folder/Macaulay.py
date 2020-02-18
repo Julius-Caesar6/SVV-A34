79e08cfb63a17fc7bcf1f81d4b891867eb68cc67
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-18 21:57:20 +0100

    Implemented macaulay integration

--- FBD_Folder/Macaulay.py
+++ FBD_Folder/Macaulay.py
@@ -1,6 +1,18 @@
-def macaulay(x,a, power):
-    if x<a:
-        return 0
-    else:
-        return (x-a)**power
+class Macaulay:
+    def __init__(self,a, b, power):
+        # Macaulay function in form: b*<x-a>**power.
+        self.a = a
+        self.b = b
+        self.power = power
+
+    def result(self, x):
+        if x < self.a:
+            return 0
+        else:
+            return self.b*((x-self.a)**self.power)
+
+    def integrate(self):
+        self.power += 1
+        self.b  = self.b / self.power
+
 
