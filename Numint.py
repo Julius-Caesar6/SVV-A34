156c9e4b8768cda00656439f11f65b4af07c8659
Nathaniel <nps.dahl@gmail.com>
2020-02-18 17:53:17 +0100

    Reworked the loop, actually works correctly now. Verified it with a straight line

--- Numint.py
+++ Numint.py
@@ -5,6 +5,9 @@ def simpson(f0, f1, f2, x0, x2):
     h=(x2-x0)/2
     return h/3 * (f0+4*f1+f2)
 
+def trapezoid(f0,f1,x0,x1):
+    return (x1-x0)/2*(f0+f1)
+
 def comp_num_int(x_lst,f_lst):
     """input coordinates and the function values at those coordinate as two lists of the same length. Include a point at the beginning and end"""
     
@@ -12,27 +15,18 @@ def comp_num_int(x_lst,f_lst):
 
     #Check length of array for compatibility with simpson
     if len(x_lst) == len(f_lst):
-        if len(x_lst)%3 == 0:
-            #no weird bits needed
-            n = len(x_lst)/3 
-            for i in range(int(n)):
-                function_sum += simpson(f_lst[i*3], f_lst[i*3+1], f_lst[i*3+2], x_lst[i*3], x_lst[i*3+2])
+        i = 0
+        while i < len(x_lst)-2:
+            function_sum += simpson(f_lst[i], f_lst[i+1], f_lst[i+2], x_lst[i], x_lst[i+2])
+            i += 2
+        if i != len(x_lst):
+            #do trap rule
+            while i != len(x_lst)-1:
+                function_sum += trapezoid(f_lst[i],f_lst[i+1],x_lst[i],x_lst[i+1])
+                i += 1
         
-        elif len(x_lst)%3 == 1:
-            #Do something
-            n = len(x_lst)/3 - 1
-            for i in range(int(n)):
-                function_sum += simpson(f_lst[i*3], f_lst[i*3+1], f_lst[i*3+2], x_lst[i*3], x_lst[i*3+2])
-
-        elif len(x_lst)%3 == 2:
-            #Do final thing
-            n = len(x_lst)/3 - 1
-            for i in range(int(n)):
-                function_sum += simpson(f_lst[i*3], f_lst[i*3+1], f_lst[i*3+2], x_lst[i*3], x_lst[i*3+2])
-
-        else
-            #throw a shit fit as this is impossible
-
+    else:
+        raise Exception('Not equal number of points as function values')
     return function_sum
 
-print(comp_num_int([0,1,2],[1,1,1]))
+print(comp_num_int([0,1,2,3,4,5,6,7,8],[1,1,1,1,1,1,1,1,1]))
