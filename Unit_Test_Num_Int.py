e188d300d62062f361a43017960e176c54754c97
Nathaniel <nps.dahl@gmail.com>
2020-02-19 09:26:05 +0100

    Added Unit test file.

---
+++ Unit_Test_Num_Int.py
@@ -0,0 +1,32 @@
+import unittest
+from Numint import simpson, trapezoid,  comp_num_int
+
+
+'''
+class Macaulaytests(unittest.TestCase):
+
+    def testxsmallera(self):
+        # test if it returns 0 if x is smaller than a
+        mac = Macaulay(5,1,1)
+        self.assertEqual(mac.result(2),0)
+
+    def testmacactive(self):
+        # test if it returns x-a if x is bigger than a.
+        mac = Macaulay(2,1,1)
+        self.assertEqual(mac.result(5),3)
+
+    def testpower(self):
+        #test if result is to the correct power
+        mac = Macaulay(2,1,2)
+        self.assertEqual(mac.result(5),9)
+
+    def testintegrate(self):
+        #test if integration works
+        mac = Macaulay(2,1,2)
+        mac.integrate()
+        self.assertEqual(mac.power, 3)
+        self.assertEqual(mac.b,1/3)
+
+if _name_ == '_main_':
+    unittest.main()
+'''
