230c122419823abd8443188eb47fdbdba18b8b08
Vladimir fayt <vfayt99@gmail.com>
2020-02-20 15:15:15 +0100

    c

--- MOI.py
+++ MOI.py
@@ -3,29 +3,37 @@ import matplotlib.pyplot as plt
 
 h = 17.3 #cm
 ca = 48.4 #cm
+stringers = 13
+hst = 1.4 #cm
+wst = 1.8 #cm
+tst = 1.2/10 #cm
+t = 1.1/10 #cm
+tspar= 2.5/10 #cm
 
 
 # plotting stringers as booms
 
 straight = np.sqrt((h / 2) ** 2 + (ca - h / 2) ** 2)  # length of straight section
 
-s = (straight + np.pi * h / 4) / 9  # distance between stringers
+s = (straight + np.pi * h / 4) / ((stringers+1)/2)  # distance between stringers
 
 g = s / (h / 2)
 h1 = g
-h2 = 2 * g
 
-# stringer op 0 deg - 33 deg - 67 deg
-z = [-h / 2, -np.cos(h1) * (h / 2), -np.cos(h2) * (h / 2)]  # stringer positions on curved section
-y = [0, np.sin(h1) * (h / 2), np.sin(h2) * (h / 2)]  # stringer positions on curved section
+
+
+
+# stringer op
+z = [-h / 2, -np.cos(h1) * (h / 2)]  # stringer positions on curved section
+y = [0, np.sin(h1) * (h / 2)]  # stringer positions on curved section
 
 alpha = h / 2 / (ca - h / 2)
 
-rest = 3 * s - np.pi * (h / 4)
+rest = 2 * s - np.pi * (h / 4)
 z.append(rest * np.cos(alpha))  # stringer positions on straight section
 y.append(h / 2 - rest * np.sin(alpha))  # stringer positions on straight section
 
-for i in range(1, 6):
+for i in range(1, int((stringers+1)/2-2)):
     z.append((rest + i * s) * np.cos(alpha))
     y.append(h / 2 - (rest + i * s) * np.sin(alpha))
 
@@ -66,21 +74,21 @@ plt.plot(c2x,c2y,"b")
 plt.show()
 
 # moment of inertia
-hst = 1.4 #cm
-wst = 1.8 #cm
-tst = 1.2/10 #cm
-t = 1.1/10 #cm
-tspar= 2.5/10 #cm
+A_stringer = tst*hst + wst*tst
+
+y_cen = (-(2*(h/2)/(np.pi))*np.pi*(h/2)*t + 2*straight*t*(ca-h/2)/2+sum(A_stringer*np.array(z[1:]))*2+A_stringer*z[0])\
+        /(np.pi*(h/2)*t+2*straight*t+tspar*h+stringers*A_stringer)
+
 Izz_stringer = 1/12*tst*hst**3
 Iyy_stringer= 1/12*tst*wst**3
-A_stringer = tst*hst + wst*tst
-Izz_spar =  1/12*tspar*h
+
+Izz_spar =  1/12*tspar*h**3
 
 d2_y = np.square(y)
-d2_z = np.square(z[1:])
+d2_z = np.square(z[1:]-y_cen)
 
 Steiner_z= 2*A_stringer*(sum(d2_y))
-Steiner_y = A_stringer*(z[0])**2 + 2*A_stringer*(sum(d2_z))
+Steiner_y = A_stringer*(z[0]-y_cen)**2 + 2*A_stringer*(sum(d2_z))
 
 Izz_circle = (np.pi * (h/2)**3 * t)/2
 Iyy_circle = Izz_circle
@@ -88,12 +96,13 @@ Iyy_circle = Izz_circle
 Izz_straight = (t*straight/12)*(straight**2 * (np.sin(alpha))**2)*2
 Iyy_straight = (t*straight/12)*(straight**2 * (np.cos(alpha))**2)*2
 
-Steiner_circle_y = (2*(h/2)/(np.pi))**2 * (np.pi*(h/2)*t)
+Steiner_circle_y = (2*(h/2)/(np.pi)+y_cen)**2 * (np.pi*(h/2)*t)
+Steiner_spar = tspar*h*(y_cen)**2
 Steiner_straight_z = 2*(straight*t)*(h/4)**2
-Steiner_straight_y = 2*(straight*t)*((ca-h/2)/2)**2
+Steiner_straight_y = 2*(straight*t)*((ca-h/2)/2-y_cen)**2
 
-Izz_total = Izz_circle + Izz_straight + Steiner_z + Izz_stringer*17 + Steiner_straight_z + Izz_spar
-Iyy_total = Iyy_circle +Iyy_straight + Steiner_y + Iyy_stringer*17 + Steiner_circle_y + Steiner_straight_y
+Izz_total = Izz_circle + Izz_straight + Steiner_z + Steiner_straight_z + Izz_spar #+ Izz_stringer*stringers
+Iyy_total = Iyy_circle +Iyy_straight + Steiner_y  + Steiner_circle_y + Steiner_straight_y+Steiner_spar #+ Iyy_stringer*stringers
 
 print(Izz_total, Iyy_total)
 
