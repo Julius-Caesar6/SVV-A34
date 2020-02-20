50c5c5a34647a11811c2e398b346b67dbd632151
EliseScheers <elise.scheers@telenet.be>
2020-02-20 17:00:56 +0100

    shearforce

---
+++ ShearForce.py
@@ -0,0 +1,13 @@
+#Shear force analysis
+from MOI import *
+import numpy as np
+
+
+Sz = 1
+Sy = 1
+
+#qb1--------------------------------------------------------------------------------------------------------------------
+#positive z values and positive y values
+range1 = np.linspace(0,np.pi,100)
+qb1 = -Sz/Iyy_total * (t*h**2/4 *(np.sin(range1)-0) + A_stringer/2*z[0] + A_stringer*z[1]) - Sy/Izz_total *(t*h**2/4*(-np.cos(range1)+1) + A_stringer/2*y[0] + A_stringer*y[1])
+
