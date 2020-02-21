f5c988943220a3941676ac275ea41e679c0a4879
Undermine28 <nps.dahl@gmail.com>
2020-02-21 15:15:39 +0100

    Finished interpolator, currently only outputs a,b,c,d values

--- interpolator.py
+++ interpolator.py
@@ -2,45 +2,58 @@
 import numpy as np
 
 class interpolate:
-    def __init__(self, data,pos):
+    def __init__(self, data, pos):
         self.data   = data
         self.n      = len(data)
-        self.h      = pos[1]-pos[0]
 
+        #Create a list of all the distances between points.
+        self.h      = np.zeros(self.n-1)
+        for i in range(self.n-1):
+            self.h[i]  = pos[i+1]-pos[i]
+
+        #Setup the matrixes
         co_matrix   = np.zeros((self.n-2,self.n-2))
-        f_matrix    = np.zeros((1,self.n-2))
+        f_matrix    = np.zeros((self.n-2,1))
 
         for i in range(self.n-2):
-            co_matrix[i][i]        = 4
+            co_matrix[i][i]         = (self.h[i]+self.h[i+1])/3
 
             if i != 0:
-                co_matrix[i][i-1]  = 1
-                co_matrix[i-1][i]  = 1
+                co_matrix[i][i-1]   = self.h[i]/6
+                co_matrix[i-1][i]   = self.h[i]/6
 
-            f_matrix[0][i]         = (data[i] -2*data[i+1] + data[i+2])*6  / self.h**2
+            #f matrix is shifted because i is defined weird due to m0 and mn being 0 and not part of the comatrixes
+            #part1 = (data[i+2] - data[i+1])/self.h[i+1]
+            #part2 = (data[i+1] - data[i])/self.h[i]
+
+            f_matrix[i][0]          = (data[i+2] - data[i+1])/self.h[i+1] - (data[i+1] - data[i])/self.h[i]
         
 
-        m_matrix    = np.linalg.inv(co_matrix).dot(f_matrix.transpose())
+        m_matrix    = np.linalg.solve(co_matrix,f_matrix)
+        #Add boundary conditions for m_matrix
+        m_matrix    = np.vstack((np.array([0]),m_matrix))
+        m_matrix    = np.vstack((m_matrix,np.array([0])))
 
-        self.abcd   = np.zeros[(4 , self.n - 1)]
+        self.abcd   = np.zeros((self.n - 1 , 4))
+        
+        
 
+    
         for i in range(self.n-1):
-            ai  = m_matrix[i+1] - m_matrix[i] / (6 * self.h)
+
+            ai  = (m_matrix[i+1] - m_matrix[i] )/ (6 * self.h[i])
             bi  = m_matrix[i] / 2
-            ci  = (self.data[i+1] - self.data[i]) / self.h - self.h / 3 * m_matrix[i] - self.h / 6 * m_matrix[i+1]
+            ci  = (self.data[i+1] - self.data[i]) / self.h[i] - self.h[i] / 3 * m_matrix[i] - self.h[i] / 6 * m_matrix[i+1]
             di  = self.data[i]
 
-            self.abcd[:][i] = [ai,bi,di,zi]
-
-
-        print(co_matrix)
-        print(f_matrix)
-        print(m_matrix)
+            self.abcd[i] = [ai,bi,di,di]
 
 
 
 
 
-data = [0.5,0.8,1,0.8,0.5]
-pos = [-1,-.5,0,.5,1]
-interpolate(data,pos)
+
+#Example 4.2 from the ANA reader
+#data = [0,0.2624,0.6419,1.0296]
+#pos = [0,0.1,0.3,0.6]
+#interpolate(data,pos)
