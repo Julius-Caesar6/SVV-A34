dd266d96c4121394ac212ab27d9307a3246f9dfe
EliseScheers <elise.scheers@telenet.be>
2020-02-21 11:54:30 +0100

    I CHANGED COORDINATE SYSTEM FOR Z!!!!!

--- ShearForce_2.py
+++ ShearForce_2.py
@@ -5,6 +5,8 @@ import numpy as np
 
 Sz = 1
 Sy = 1
+z_sc = 1
+G = 1
 
 #qb1--------------------------------------------------------------------------------------------------------------------
 #positive z values and positive y values
@@ -34,3 +36,41 @@ qb5 = qb4[-1] - Sy/Izz_total*(tspar*range5**2/2)
 #qb6 -------------------------------------------------------------------------------------------------------------------
 range6 = np.linspace(-np.pi/2,0,100)
 qb6 = qb4[-1] - qb5[-1] - Sz/Iyy_total*(t*h**2/4*np.sin(range6) + A_stringer/2*z[0] + A_stringer*z[1]) - Sy/Izz_total*(t*h**2/2*np.cos(range6) - A_stringer/2*y[0] - A_stringer*y[1])
+
+#moment around point 0 -------------------------------------------------------------------------------------------------
+integral1 = 1 #integral of qb1*(h/2)**2*dTheta from 0 to pi/2
+integral2 =1 #integral of qb3*8.447*ds from 0 to straight
+
+#0 = Sy*z_sc + 2*integral1 + 2*integral2 + 2*Am_cell1*qso1 + 2*Am_cell2*qso2 #equation1
+
+#rate of twist----------------------------------------------------------------------------------------------------------
+#cell 1
+integral3= 1 #integral of qb1*h/2*dtheta from 0 to pi/2
+integral4 = 1 #integral of qb2*-dy from h/2 to 0
+integral5 = 1 #integral of qb5*-dy from 0 to -h/2
+integral6 = 1#integral of qb6*h/2*dtheta from -pi/2 to 0
+
+#G*dtheta = 1/(2*Am_cell1) *((integral3+qso1)/t + (integral4+qso1-qso2)/tspar + (integral5+qso1-qso2)/tspar + (integral6+qso1)/t)  #equation2
+
+#cell2
+integral7 = 1 #integral of qb2*dy from 0 to h/2
+integral8 =1 #integral of qb3*ds from 0 to straight
+integral9 =1 #integral of qb4*ds from 0 to straight
+integral10=1 #integral of qb5*dy from -h/2 to straight
+
+#G*dtheta = 1/(2*Am_cell2)*((integral7+qso2-qso1)/tspar + (integral8+qso2)/t + (integral9+qso2)/t + (integral10+qso2-qso1)/tspar) #equation3
+
+#solving matrix of 3 equations
+a = np.array( [[2*Am_cell1,2*Am_cell2,0], [1/(2*Am_cell1)*(1/t + 1/tspar + 1/tspar +1/t), 1/(2*Am_cell1)*(-1/tspar - 1/tspar), -G],[1/(2*Am_cell2)*(-1/tspar -1/tspar), 1/(2*Am_cell2)*(1/tspar+ 1/t + 1/t + 1/tspar), -G]])
+b = np.array([-Sy*z_sc - 2*integral1 - 2*integral2, -1/(2*Am_cell1)*(integral3/t + integral4/tspar + integral5/tspar + integral6/tspar), -1/(2*Am_cell2)*(integral7/tspar + integral8/t + integral9/t +integral10/tspar)])
+matrix_answer = np.linalg.solve(a,b) #0th entry qso1, 1th entry qso2, 2nd entry, dtheta/dz
+
+#total shear flow distributions
+qb1_total = qb1 + matrix_answer[0]
+qb2_total = qb2 - matrix_answer[0] + matrix_answer[1]
+qb3_total = qb3 + matrix_answer[1]
+qb4_total = qb4 + matrix_answer[1]
+qb5_total = qb5 - matrix_answer[0] + matrix_answer[1]
+qb6_total = qb6 + matrix_answer[0]
+
+
