e3272a1b51f8f44f6cee9781ef590266e1a1d46a
EliseScheers <elise.scheers@telenet.be>
2020-02-21 14:26:50 +0100

    update shear

--- ShearForce_2.py
+++ ShearForce_2.py
@@ -4,14 +4,14 @@ import numpy as np
 
 z = np.dot(z,-1)
 
-Sz = 1
+Sz = 0
 Sy = 1
 z_sc = 1
 G = 1
 
 #qb1--------------------------------------------------------------------------------------------------------------------
 #positive z values and positive y values
-range1 = np.linspace(0,np.pi,100)
+range1 = np.linspace(0,np.pi/2,100)
 qb1 = -Sz/Iyy_total * (t*h**2/4 *(np.sin(range1)-0) + A_stringer/2*z[0] + A_stringer*z[1]) - Sy/Izz_total *(t*h**2/4*(-np.cos(range1)+1) + A_stringer/2*y[0] + A_stringer*y[1])
 
 #qb2 -------------------------------------------------------------------------------------------------------------------
@@ -40,7 +40,7 @@ qb6 = qb4[-1] - qb5[-1] - Sz/Iyy_total*(t*h**2/4*np.sin(range6) + A_stringer/2*z
 
 #moment around point 0 -------------------------------------------------------------------------------------------------
 integral1 = 1 #integral of qb1*(h/2)**2*dTheta from 0 to pi/2
-integral2 =1 #integral of qb3*8.447*ds from 0 to straight
+integral2 =1 #integral of qb3*cos(alpha)*h/2*ds from 0 to straight
 
 #0 = Sy*z_sc + 2*integral1 + 2*integral2 + 2*Am_cell1*qso1 + 2*Am_cell2*qso2 #equation1
 
