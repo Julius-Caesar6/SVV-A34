7c00a729fd5fb07dd047cb7281228efe64d542d9
Vladimir fayt <vfayt99@gmail.com>
2020-02-21 14:51:48 +0100

    c

--- SC.py
+++ SC.py
@@ -16,7 +16,7 @@ q01_c1 = h*np.pi/(2*t) + h/tspar
 q02_c1 = -h/tspar
 
 c1 = 1/Izz_total * t*h**2/4
-r1 = 1/Izz_total*(-A_stringer*(y[1])  -  t*h**4/4)
+r1 = 1/Izz_total*(-A_stringer*(y[1])  -  t*h**2/4)
 R1 = h/t*(c1+r1*np.pi/2)
 
 c2 = -tspar/Izz_total/2
@@ -32,7 +32,7 @@ qb1 = r1
 qb2 = c2*(h/2)**2
 r3 = qb1 + qb2 - 1/Izz_total * A_stringer* sum(y[2:])
 c31 = -1/Izz_total*(t*h/2)
-c32 = -1/Izz_total*(-t*h/straight/2)
+c32 = -1/Izz_total*(-t*h/straight/4)
 E2 = 2/t*(straight*r3  +  c31*straight**2/2  +  c32*straight**3/3)
 
 eq1 = [q01_c1,q02_c1,R1+R2]
@@ -52,4 +52,4 @@ print(F1,F2)
 sc = 2*(F1+F2) + 2*q01*Am_cell1  +  2*q02*Am_cell2
 print(sc)
 
-print(r1)
+qb3 = r3+c31*straight+c32*straight**2
