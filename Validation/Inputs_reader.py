58a52aa2653f46d56c16d20e3bbae348bcdbbc0c
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-22 15:27:37 +0100

    Create inputs dataframe

---
+++ Validation/Inputs_reader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+# Create a dataframe called "inputs" which contains columns:
+# node, x, y, z which shows the x,y,z coordinates of each node
+inputs = pd.read_csv('nodes_coords.csv')
+print(inputs)
+
