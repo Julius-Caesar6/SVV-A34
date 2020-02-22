76c652f4065b49c658e9ad0a8b0bf80b2b210e39
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-22 16:10:07 +0100

    Made stress csv data files

--- Validation/Inputs_reader.py
+++ Validation/Inputs_reader.py
@@ -4,5 +4,5 @@ import pandas as pd
 # Create a dataframe called "inputs" which contains columns:
 # node, x, y, z which shows the x,y,z coordinates of each node
 inputs = pd.read_csv('nodes_coords.csv')
-print(inputs)
+
 
---
+++ Validation/Outputs_reader..py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+Bending_stress_R1 = pd.read_csv('Bending_stresses_reg_1.csv',  delimiter ="\s+" )
+Bending_stress_R2 =
