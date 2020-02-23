9d561173b61ba97d75c6e18c7dab70f3910e9057
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-23 11:14:53 +0100

    defined functions for elements to nodes and nodes to coordinates

--- Validation/Data_plotter.py
+++ Validation/Data_plotter.py
@@ -4,11 +4,26 @@ import matplotlib.pyplot as plt
 from Validation.Inputs_reader import Elements
 from Validation.Inputs_reader import inputs
 
+def select_elements(stress_df, Elements):
+    TF_df = Elements['Element'].isin(stress_df['Element'])
+    return Elements.loc[TF_df]
+
+def coords_from_nodes (nodes_df, inputs):
+    TF_df = inputs['Node'].isin(nodes_df)
+    return inputs.loc[TF_df]
+
+
+
 
 # Stressese
 s1 = Bending_stress_R1
-elements = s1['Element']
+elements = s1['Element'] # All elements covered in output
+# Max = 6634, len = 5778
+
+R = Elements['Element'].isin(elements)
+U = Elements.loc[R]
+print(U)
 
 
-data  = inputs.loc[inputs['Node'].isin(nodes)]
-print(inputs['Node'].max(), inputs['Node'].shape)
+
+
