aa049b04b63dc4cd3e989ce98bc0a79301babd47
Nathaniel <nps.dahl@gmail.com>
2020-02-24 15:22:16 +0100

    Fixed a bunch of small errors in the unit test document including changing all the function to start with 'test' so that they all actually run

--- Unit_Test_Num_Int.py
+++ Unit_Test_Num_Int.py
@@ -14,18 +14,18 @@ class int_test(unittest.TestCase):
         area = (1/4*x2**4 + 1/3*x2**3 + 1/2*x2**2+ x2) - (1/4*x0**4 + 1/3*x0**3 + 1/2*x0**2+ x0)
         self.assertEqual(simpson(fun0,fun1,fun2,x0,x2),area)
 
-    def trap_test(self):
+    def test_trap(self):
         #testing the trapezoid rule with a linear case
         x0=1
-        fun0=2*x+3
+        fun0=2*x0+3
         x1=3
-        fun1=2*x+3
-        area = (1/2*x1**2 +3*x1) - (1/2*x0**2 +3*x0)
-        self.assertEqual(trapezoid(fun0.fun1,x0,x1),area)
+        fun1=2*x1+3
+        area = (x1**2 +3*x1) - (x0**2 +3*x0)
+        self.assertEqual(trapezoid(fun0,fun1,x0,x1),area)
 
 class comp_test(unittest.TestCase):
 
-    def comp_lin_even(self):
+    def test_comp_lin_even(self):
         #testing the com num int function with even linear function
         x_lst=np.linspace(1,10,10)
         f_lst = []
@@ -34,78 +34,51 @@ class comp_test(unittest.TestCase):
         area = (3/2*x_lst[-1]**2 + 4*x_lst[-1]) - (3/2*x_lst[0]**2 + 4*x_lst[0])
         self.assertEqual(comp_num_int(x_lst,f_lst),area)
 
-    def comp_lin_odd(self):
+    def test_comp_lin_odd(self):
         #testing the com num int function with odd linear function
-        x_lst=numpy.linspace(1,15,15)
+        x_lst=np.linspace(1,15,15)
         f_lst=[]
         for i in x_lst:
             f_lst.append(5*i+6)
         area = (5/2*x_lst[-1]**2 + 6*x_lst[-1]) - (5/2*x_lst[0]**2 + 6*x_lst[0])
         self.assertEqual(comp_num_int(x_lst,f_lst),area)
 
-    def comp_polythree_even(self):
+    def test_comp_polythree_even(self):
         #testing the com num int function with even amount of values on  polynomial to the power 3
-        x_lst=np.linspace(1,10,10)
+        x_lst=np.linspace(1,10,100)
         f_lst=[]
         for i in x_lst:
             f_lst.append(3*i**3 + 4*i**2 + 5)
         area = (3/4*x_lst[-1]**4 + 4/3*x_lst[-1]**3 + 5*x_lst[-1])-(3/4*x_lst[0]**4 + 4/3*x_lst[0]**3 + 5*x_lst[0])
-        self.assertEqual(comp_num_int(x_lst,f_lst),area)
+        self.assertAlmostEqual(comp_num_int(x_lst,f_lst),area, delta = 1)
 
-    def comp_polythree_odd(self):
+    def test_comp_polythree_odd(self):
         #testing the com num int function with odd amount of values on  polynomial to the power 3
-        x_lst=numpy.linspace(1,15,15)
+        x_lst=np.linspace(1,15,15)
         f_lst=[]
         for i in x_lst:
             f_lst.append(3*i**3 + 4*i**2 + 5)
         area = (3/4*x_lst[-1]**4 + 4/3*x_lst[-1]**3 + 5*x_lst[-1])-(3/4*x_lst[0]**4 + 4/3*x_lst[0]**3 + 5*x_lst[0])
         self.assertEqual(comp_num_int(x_lst,f_lst),area)
 
-    def comp_cool_even(self):
+    def test_comp_cool_even(self):
         #testing the com num int function with even number of values but with a cool function!
-        x_lst= np.linspace(1,10,100)
+        x_lst= np.linspace(1,10,300)
         f_lst= []
         for i in x_lst:
-            f_lst.append(np.exp(i) + np.sin(i)+ x**5)
+            f_lst.append(np.exp(i) + np.sin(i)+ i**5)
         area = (np.exp(x_lst[-1]) - np.cos(x_lst[-1]) + 1/6*x_lst[-1]**6) - (np.exp(x_lst[0]) - np.cos(x_lst[0]) + 1/6*x_lst[0]**6)
-        self.assertEqual(comp_num_int(x_lst, f_lst), area)
+        self.assertAlmostEqual(comp_num_int(x_lst, f_lst), area, delta=1)
 
-    def comp_cool_odd(self):
+    def test_comp_cool_odd(self):
         #testing the com num int function with even (poly 3 more or less exact) number of point and odd (linear parts exact, )
-        x_lst = numpy.linspace(1,15,150)
+        x_lst = np.linspace(1,15,800)
         f_lst= []
         for i in x_lst:
             f_lst.append(2*np.exp(i) + 4*i*np.cos(i)+ i**5)
-        area = (2*np.exp(x_lst[-1]) + np.cos(x_lst[-1]) + 4*x*np.sin(x_lst[-1]) + 1/6*x_lst[-1]**6) - (2*np.exp(x_lst[0]) + np.cos(x_lst[0]) + 4*x*np.sin(x_lst[0]) + 1/6*x_lst[0]**6)
-        self.assertEqual(comp_num_int(x_lst, f_lst), area)
+        area = (2*np.exp(x_lst[-1]) + np.cos(x_lst[-1]) + 4*x_lst[-1]*np.sin(x_lst[-1]) + 1/6*x_lst[-1]**6) - (2*np.exp(x_lst[0]) + np.cos(x_lst[0]) + 4*x_lst[0]*np.sin(x_lst[0]) + 1/6*x_lst[0]**6)
+        self.assertAlmostEqual(comp_num_int(x_lst, f_lst), area, delta= 1)
+
 
 if __name__ == '__main__':
     unittest.main()
-'''
-class Macaulaytests(unittest.TestCase):
-
-    def testxsmallera(self):
-        # test if it returns 0 if x is smaller than a
-        mac = Macaulay(5,1,1)
-        self.assertEqual(mac.result(2),0)
-
-    def testmacactive(self):
-        # test if it returns x-a if x is bigger than a.
-        mac = Macaulay(2,1,1)
-        self.assertEqual(mac.result(5),3)
-
-    def testpower(self):
-        #test if result is to the correct power
-        mac = Macaulay(2,1,2)
-        self.assertEqual(mac.result(5),9)
-
-    def testintegrate(self):
-        #test if integration works
-        mac = Macaulay(2,1,2)
-        mac.integrate()
-        self.assertEqual(mac.power, 3)
-        self.assertEqual(mac.b,1/3)
-
-if _name_ == '_main_':
-    unittest.main()
-'''
