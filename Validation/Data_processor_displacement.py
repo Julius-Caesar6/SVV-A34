fb0100dfd10267c8ae396b5e3875415ddb772cd9
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-25 14:17:39 +0100

    something

--- Validation/Data_processor_displacement.py
+++ Validation/Data_processor_displacement.py
@@ -26,12 +26,3 @@ cords['ynew'] = cords['yold'] + Bending_disp['U.U2']
 cords['znew'] = cords['zold'] + Bending_disp['U.U3']
 
 Bending_displacement = cords[['Node','xnew','ynew','znew']]
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-# ax.set_aspect('equal')
-p = ax.scatter(Bending_displacement['xnew'], Bending_displacement['ynew'], Bending_displacement['znew'])
-# ax.set_xlim3d(0,2500)
-# ax.set_ylim3d(-1250,1250)
-# ax.set_zlim3d(-1000,1000)
-plt.show()
