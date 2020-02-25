243635c86458dc4246204b59cb3f07d772493196
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-25 15:30:54 +0100

    Created displacement plots

--- Validation/Data_plotting.py
+++ Validation/Data_plotting.py
@@ -10,15 +10,52 @@ Jam_Bent_stress_R2 = pd.read_csv('Stress_plots/Jam_Bent_sr2.csv')
 Jam_Straight_stress_R1 = pd.read_csv('Stress_plots/Jam_straight_sr1.csv')
 Jam_Straight_stress_R2 = pd.read_csv('Stress_plots/Jam_straight_sr2.csv')
 
-from matplotlib import cm
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-# ax.set_aspect('equal')
-p = ax.scatter(Bending_stress_R1['intpointx'], Bending_stress_R1['intpointy'], Bending_stress_R1['intpointz'],c=Bending_stress_R1['S.Mises.mean'])
-q = ax.scatter(Bending_stress_R2['intpointx'], Bending_stress_R2['intpointy'], Bending_stress_R2['intpointz'], c = Bending_stress_R2['S.Mises.mean'])
-fig.colorbar(p)
-ax.xlabel()
-ax.set_xlim3d(0,2500)
-ax.set_ylim3d(-1250,1250)
-ax.set_zlim3d(-1000,1000)
-plt.show()
+# Load Displacement DataFrames
+Bending_disp = pd.read_csv('Displacement_plots/Bending_disp.csv')
+Bending_disp_assembly =  pd.read_csv('Displacement_plots/Bending_disp_assembly.csv')
+Jam_bent_disp = pd.read_csv('Displacement_plots/Jam_Bent_disp.csv')
+Jam_bent_disp_assembly =  pd.read_csv('Displacement_plots/Jam_Bent_disp_assembly.csv')
+Jam_straight_disp = pd.read_csv('Displacement_plots/Jam_straight_disp.csv')
+Jam_straight_disp_assembly =  pd.read_csv('Displacement_plots/Jam_straight_disp_assembly.csv')
+
+def plot_displacement_figure(disp_df, disp_df_assembly):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    p = ax.scatter(disp_df['xnew'], disp_df['ynew'], disp_df['znew'])
+    q = ax.scatter(disp_df_assembly['xnew'], disp_df_assembly['ynew'], disp_df_assembly['znew'])
+    ax.set_xlabel('x [mm]')
+    ax.set_ylabel('y [mm]')
+    ax.set_zlabel('z [mm]')
+
+    # ax.set_xlim3d(0, 2500)
+    # ax.set_ylim3d(-1000,1000)
+    # ax.set_zlim3d(-600,600)
+    plt.show()
+    plt.close()
+
+def plot_stress_figure(stress_dfr1, stress_dfr2, stress_type):
+    if stress_type == 'Shear':
+        type = 'S.S12.mean'
+    elif stress_type == 'VMises':
+        type = 'S.Mises.mean'
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    # ax.set_aspect('equal')
+    p = ax.scatter(stress_dfr1['intpointx'], stress_dfr1['intpointy'], stress_dfr1['intpointz'],c=stress_dfr1[type], cmap='jet')
+    q = ax.scatter(stress_dfr2['intpointx'], stress_dfr2['intpointy'], stress_dfr2['intpointz'], c = stress_dfr2[type], cmap='jet')
+    clb = fig.colorbar(p)
+    ax.set_xlabel('x [mm]')
+    ax.set_ylabel('y [mm]')
+    ax.set_zlabel('z [mm]')
+    clb.set_label(stress_type)
+    ax.set_xlim3d(600, 2000)
+    ax.set_ylim3d(-200,200)
+    ax.set_zlim3d(-600,600)
+    plt.show()
+    plt.close()
+
+
+plot_displacement_figure(Bending_disp, Bending_disp_assembly)
+plot_stress_figure(Bending_stress_R1, Bending_stress_R2, 'VMises')
--- Validation/Data_processor_displacement.py
+++ Validation/Data_processor_displacement.py
@@ -7,22 +7,36 @@ from mpl_toolkits import mplot3d
 def coords_from_nodes (node, inputs):
     return inputs.loc[inputs['Node']==node]
 
-X = []
-Y = []
-Z = []
 
-for index, row in Bending_disp.iterrows():
-    X.append(float(coords_from_nodes(row['Node'], inputs)['x']))
-    Y.append(float(coords_from_nodes(row['Node'], inputs)['y']))
-    Z.append(float(coords_from_nodes(row['Node'], inputs)['z']))
+def prepare_displacementdf_for_plotting(Disp_df):
+    X = []
+    Y = []
+    Z = []
 
-cords = pd.DataFrame(Bending_disp['Node'])
-cords['xold'] = X
-cords['yold'] = Y
-cords['zold'] = Z
+    for index, row in Disp_df.iterrows():
+        X.append(float(coords_from_nodes(row['Node'], inputs)['x']))
+        Y.append(float(coords_from_nodes(row['Node'], inputs)['y']))
+        Z.append(float(coords_from_nodes(row['Node'], inputs)['z']))
 
-cords['xnew'] = cords['xold'] + Bending_disp['U.U1']
-cords['ynew'] = cords['yold'] + Bending_disp['U.U2']
-cords['znew'] = cords['zold'] + Bending_disp['U.U3']
+    cords = pd.DataFrame(Disp_df['Node'])
+    cords['xold'] = X
+    cords['yold'] = Y
+    cords['zold'] = Z
 
-Bending_displacement = cords[['Node','xnew','ynew','znew']]
+    cords['xnew'] = cords['xold'] + Disp_df['U.U1']
+    cords['ynew'] = cords['yold'] + Disp_df['U.U2']
+    cords['znew'] = cords['zold'] + Disp_df['U.U3']
+
+    return cords[['Node','xnew','ynew','znew']]
+
+
+prepare_displacementdf_for_plotting(Bending_disp).to_csv('Bending_disp.csv')
+prepare_displacementdf_for_plotting(Bending_disp_assembly).to_csv('Bending_disp_assembly.csv')
+
+prepare_displacementdf_for_plotting(Jam_Bent_disp).to_csv('Jam_Bent_disp.csv')
+prepare_displacementdf_for_plotting(Jam_Bent_disp_assembly).to_csv('Jam_Bent_disp_assembly.csv')
+
+prepare_displacementdf_for_plotting(Jam_Straight_disp).to_csv('Jam_straight_disp.csv')
+prepare_displacementdf_for_plotting(Jam_Straight_disp_assembly).to_csv('Jam_straight_disp_assembly.csv')
+
+
