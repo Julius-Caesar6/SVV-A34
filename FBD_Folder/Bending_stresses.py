36ff9288b6a2a3511f666dc3ef32eb1c0dde3201
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-27 13:04:54 +0100

    started plot preparation

--- FBD_Folder/Bending_stresses.py
+++ FBD_Folder/Bending_stresses.py
@@ -1,6 +1,11 @@
+from FBD_Folder.Equations import *
+import numpy as np
+from FBD_Folder.Constants import *
+
+def sigmaxx(y,z, x, Iyy, Izz):
+    return (My(x)*z/Iyy) - (Mz(x)*y/Izz) # FIXME check if two terms should be added or subtracted
 
 
 
+xrange = np.linspace(0, la, 50)
 
-def sigmaxx(y,z, My, Mz, Iyy, Izz):
-    return (My*z/Iyy) - (Mz*y/Izz) # FIXME check if two terms should be added or subtracted
