8a1d537ca981f473b727d773a3b3e51179b45fbc
Nathaniel <nps.dahl@gmail.com>
2020-02-28 13:42:48 +0100

    added 2Dplots

--- Validation/Data_plotting.py
+++ Validation/Data_plotting.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from mpl_toolkits import mplot3d
+import matplotlib.style as style
+style.use('seaborn-talk') #sets the size of the charts
+style.use('ggplot')
 
 # Load Stress DataFrames
 Bending_stress_R1 = pd.read_csv('Stress_plots/Bending_sr1.csv')
@@ -39,6 +42,22 @@ def plot_displacement_figure(disp_df, disp_df_assembly):
     plt.show()
     plt.close()
 
+def plot_displacement_figure_2D(disp_df, disp_df_assembly):
+    fig = plt.figure()
+    ax = plt.axes()
+    
+    p = ax.plot(disp_df['xnew'], disp_df['ynew'],'b.')
+    # q = ax.scatter(disp_df_assembly['xnew'], disp_df_assembly['ynew'], disp_df_assembly['znew'])
+    ax.set_xlabel('x [mm]')
+    ax.set_ylabel('y [mm]')
+    # ax.set_zlabel('z [mm]')
+
+    ax.set_xlim(0, 2500)
+    ax.set_ylim(0,20)
+    # ax.set_zlim3d(-600,600)
+    plt.show()
+    plt.close()
+
 
 def plot_stress_figure(stress_dfr1, stress_dfr2, stress_type):
     if stress_type == 'Shear':
@@ -55,7 +74,7 @@ def plot_stress_figure(stress_dfr1, stress_dfr2, stress_type):
     ax.set_xlabel('x [mm]')
     ax.set_ylabel('y [mm]')
     ax.set_zlabel('z [mm]')
-    clb.set_label(stress_type)
+    clb.set_label(stress_type+" N/m2")
     ax.set_xlim3d(600, 2000)
     ax.set_ylim3d(-200,200)
     ax.set_zlim3d(-600,600)
@@ -64,4 +83,11 @@ def plot_stress_figure(stress_dfr1, stress_dfr2, stress_type):
 
 
 
-plot_displacement_figure(Bending_disp_spar, 1)
+plot_displacement_figure(Jam_bent_disp, 1)
+
+
+plot_displacement_figure_2D(Bending_disp_spar,1)
+plot_displacement_figure_2D(Jam_bent_disp,1)
+plot_displacement_figure_2D(Jam_straight_disp,1)
+
+#plot_stress_figure(Jam_Straight_stress_R1,Jam_Straight_stress_R2,'VMises')
