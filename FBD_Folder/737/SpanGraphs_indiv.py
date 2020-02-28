d3a01e6094ae60b8554aa84b6e5c08ee8ae0a7f0
Julius-Caesar6 <60960305+Julius-Caesar6@users.noreply.github.com>
2020-02-28 14:55:33 +0100

    737disp to csv

--- FBD_Folder/737/SpanGraphs_indiv.py
+++ FBD_Folder/737/SpanGraphs_indiv.py
@@ -2,7 +2,7 @@ from Equations import *
 import numpy as np
 from Constants import *
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 topval = 70
 x = np.linspace(0.0,la,topval)
@@ -12,7 +12,23 @@ for i in range(len(x)):
         print(int(100*i/topval),'%')
     sel.append(vz(x[i]))
 
-#
+
+
+VZ_737 = pd.DataFrame({'xvalues' : x,
+                        'vzdisp' : sel,
+                        })
+
+topval = 70
+x = np.linspace(0.0,la,topval)
+sel2 = []
+for i in range(len(x)):
+    if int(i%(5/(100/topval))) == 0:
+        print(int(100*i/topval),'%')
+    sel2.append(vy(x[i]))
+
+VZ_737['vydisp'] = sel2
+VZ_737.to_csv('737hingelinedeflections.csv')
+
 # print(Tx(0.1))
 #
 # y2 = []
@@ -28,7 +44,7 @@ plt.axvline(x2+0.5*xa,label='x_P',color='g',linestyle='--',linewidth=0.5)
 
 plt.title('Title')
 plt.xlabel('x [m]')
-plt.ylabel('S [N]')
+plt.ylabel('y [m]')
 plt.plot(x,sel)
 
 #plt.legend()
