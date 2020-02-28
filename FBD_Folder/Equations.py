5ddf33826a4ed4fe4e0aefd3f7bcb6b2a78c68ff
maxrob27 <44088162+maxrob27@users.noreply.github.com>
2020-02-28 09:28:09 +0100

    Final FBD

--- FBD_Folder/Equations.py
+++ FBD_Folder/Equations.py
@@ -34,11 +34,11 @@ def Mz(x):
 def Tx(x):
     return Macaulay(x1, Ry1*-zhat, 0).result(x) + Macaulay(x2, Ry2*-zhat, 0).result(x) + Macaulay(x3,Ry3*-zhat,0).result(x) + -1*(Macaulay(x2-(xa/2), Rj*((ha/2)-zhat)*np.sin(beta),0).result(x) + Macaulay(x2-(xa/2),-1*Rj*np.cos(beta)*(ha/2),0).result(x) + Macaulay(x2+(xa/2), -1*P*np.cos(beta)*(ha/2), 0).result(x) + Macaulay(x2+(xa/2), P*np.sin(beta)*((ha/2)-zhat), 0).result(x)) +Iswitch*IntegrateX(x,1,1)-zhat*Iswitch*IntegrateX(x,1,0)
 
-def Sy(x): #check
-    return Macaulay(x1,-Ry1,0).result(x) + Macaulay(x2,-Ry2,0).result(x) + Macaulay(x3,-Ry3,0).result(x) + Macaulay(x2-0.5*xa,-Rj*np.sin(beta),0).result(x) + Macaulay(x2+0.5*xa,-P*np.sin(beta),0).result(x) -  Iswitch*IntegrateX(x,1,0)
+# def Sy(x): #check
+#     return Macaulay(x1,-Ry1,0).result(x) + Macaulay(x2,-Ry2,0).result(x) + Macaulay(x3,-Ry3,0).result(x) + Macaulay(x2-0.5*xa,-Rj*np.sin(beta),0).result(x) + Macaulay(x2+0.5*xa,-P*np.sin(beta),0).result(x) -  Iswitch*IntegrateX(x,1,0)
 
-def Sz(x): #check
-    return Macaulay(x1,Rz1,0).result(x) + Macaulay(x2,Rz2,0).result(x) + Macaulay(x3,Rz3,0).result(x) + Macaulay(x2-0.5*xa,Rj*np.cos(beta),0).result(x) + Macaulay(x2+0.5*xa,P*np.cos(beta),0).result(x)
+# def Sz(x): #check
+#     return Macaulay(x1,Rz1,0).result(x) + Macaulay(x2,Rz2,0).result(x) + Macaulay(x3,Rz3,0).result(x) + Macaulay(x2-0.5*xa,Rj*np.cos(beta),0).result(x) + Macaulay(x2+0.5*xa,P*np.cos(beta),0).result(x)
 
 def vy(x):
     return (-1/(E*Izz))*(-Iswitch*IntegrateX(x,4,0) + Macaulay(x1,-Ry1/6,3).result(x)+ Macaulay(x2,-Ry2/6,3).result(x) +  Macaulay(x3,-Ry3/6,3).result(x)  + Macaulay(x2+0.5*xa,-P*np.sin(beta)/6,3).result(x)  + Macaulay(x2-0.5*xa,-Rj*np.sin(beta)/6,3).result(x)  ) + C1*x + C2
@@ -56,16 +56,12 @@ def vzder(x):  #CHECK
     return (-1/(E*Iyy)) * ( Macaulay(x1,Rz1/2,2).result(x) + Macaulay(x2,Rz2/2,2).result(x) + Macaulay(x3,Rz3/2,2).result(x) +  Macaulay(x2-0.5*xa,Rj*np.cos(beta)/2,2).result(x) +  Macaulay(x2+0.5*xa,P*np.cos(beta)/2,2).result(x)   )  + C3
 
 
-print(My(0.0),Mz(0.0),Sy(0.0),Sz(0.0))
-
 dx = 0.0001
 def Mz0(x):
     return Macaulay(x1,-Ry1,1).result(x) + Macaulay(x2,-Ry2,1).result(x) + Macaulay(x3,-Ry3,1).result(x) + Macaulay(x2-0.5*xa,-Rj*np.sin(beta),1).result(x) + Macaulay(x2+0.5*xa,-P*np.sin(beta),1).result(x)
 
-def Sy2(x):
-    return (-Mz0(x)+Mz0(x+dx))/dx  + Iswitch*IntegrateX(x,1,0)
+def Sy(x):
+    return (-Mz0(x)+Mz0(x+dx))/dx  - Iswitch*IntegrateX(x,1,0) +  2*Iswitch*IntegrateX(la,1,0)*Macaulay(x2,1,0).result(x)
 
-def Sz2(x):
+def Sz(x):
     return (-My(x)+My(x+dx))/dx
-
-print(Sy2(0),Sz2(0),Sy2(3),Sz2(la))
